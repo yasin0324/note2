e5a84f7461fc0c67e2b440d365706384dbc43f9c
yasin <1848737490@qq.com>
2025-04-03 22:17:02 +0800

    25.04.03

--- 训练营/code/第二次课/test.js
+++ 训练营/code/第二次课/test.js
@@ -1,6 +1,29 @@
-let sum = 0
-let arr = [11,22,33]
-for(let i = 0;i <= arr.length; i++){
-	sum += arr[i]
-}
-console.log(sum)
+let nums = [1, 3, -1, -3, 5, 3, 6, 7];
+let k = 3;
+
+var maxSlidingWindow = function (nums, k) {
+    const queue = [];
+    const res = [];
+
+    let i = 0;
+    for (; i < k; i++) {
+        while (queue.length && queue[queue.length - 1] < nums[i]) {
+            queue.shift();
+        }
+        queue.push(nums[i]);
+    }
+    res.push(queue[0]);
+
+    while (i < nums.length) {
+        queue.shift();
+        while (queue.length && queue[queue.length - 1] < nums[i]) {
+            queue.shift();
+        }
+        queue.push(nums[i]);
+        res.push(queue[0]);
+    }
+
+    return res;
+};
+
+console.log(maxSlidingWindow(nums, k));
